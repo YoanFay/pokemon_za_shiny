3aa87d7092a80eae5bed6b7e5e81dec501900b58
YoanFay <yoanfayolle.yf@gmail.com>
2026-02-10 16:46:09 +0100

    build(db): create db

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import express, { response } from 'express'
+import mariadb from "mariadb";
+import { loadEnvFile } from 'node:process'
+
+loadEnvFile('./.env');
+
+const connectionPool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: process.env.CONNECTION_LIMIT_AMOUNT
+});
+
+let connection;
 
 const app = express();
 
